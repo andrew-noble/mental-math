61b98789ac0ab65b2ae41c6879a61c838b642f8f
Andrew Noble <aknoble.andrew@gmail.com>
2025-01-19 12:35:26 -0900

    More basic styling

--- src/components/NumberPad.jsx
+++ src/components/NumberPad.jsx
@@ -5,18 +5,31 @@ export default function NumberPad({
 }) {
   return (
     <>
-      {Array.from({ length: 10 }, (_, i) => (
-        <button key={i} onClick={() => handleButtonClick(i)}>
-          {i}
+      <div className="p-3 grid grid-cols-3 gap-4 w-max">
+        {Array.from({ length: 10 }, (_, i) => (
+          <button
+            key={i}
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600"
+            onClick={() => handleButtonClick(i)}
+          >
+            {i}
+          </button>
+        ))}
+      </div>
+      <div>
+        <button
+          className="bg-yellow-500 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600 m-3"
+          onClick={handleBackspace}
+        >
+          Backspace
         </button>
-      ))}
-      <button
-        className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600"
-        onClick={handleBackspace}
-      >
-        Backspace
-      </button>
-      <button onClick={handleSubmit}>Submit</button>
+        <button
+          className="bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 m-3"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
     </>
   );
 }
--- src/components/PromptBar.jsx
+++ src/components/PromptBar.jsx
@@ -1,3 +1,3 @@
 export default function PromptBar({ prompt }) {
-  return <h1>{prompt}</h1>;
+  return <h1 className="p-5">{prompt}</h1>;
 }
