6088b651255cae87ce7ce1dc568644ae3005a66f
Andrew Noble <aknoble.andrew@gmail.com>
2025-02-04 21:56:14 -0900

    Implement stats -- not pretty but a start. I need an SQL db tbh

--- src/components/QuestionStat.jsx
+++ src/components/QuestionStat.jsx
@@ -1,7 +1,16 @@
-export default function QuestionStat({ question }) {
+export default function QuestionStat({
+  question,
+  total,
+  correct,
+  averageTime,
+}) {
+  const accuracyPct = (correct / total) * 100;
+
   return (
     <div>
       <p>{question.id}</p>
+      <p>Accuracy (correct/total): {accuracyPct.toFixed(2)}%</p>
+      <p>Average time: {averageTime}s</p>
     </div>
   );
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import questions from "../questions.json";
 import QuestionStat from "./QuestionStat";
+import { getItem } from "../services/localStorage";
 
 const questionList = questions.questions;
 
 export default function Stats({ module }) {
-  const [activeModule, setActiveModule] = useState(module);
-
   //get questions for the active module
   const moduleQuestions = questionList.filter((q) => q.type === module);
+
+  const userStats = getItem("userStats") || {};
 
   //fix this later to use ui components like from shadcn rather than manual conditional rendering and state
   return (
@@ -16,13 +17,25 @@ export default function Stats({ module }) {
       {module === "multiplication" ? (
         <>
           {moduleQuestions.map((question) => (
-            <QuestionStat key={question.id} question={question} />
+            <QuestionStat
+              key={question.id}
+              question={question}
+              total={userStats[question.id]?.total || 0}
+              correct={userStats[question.id]?.correct || 0}
+              averageTime={userStats[question.id]?.averageTime || 0}
+            />
           ))}
         </>
       ) : (
         <>
           {moduleQuestions.map((question) => (
-            <QuestionStat key={question.id} question={question} />
+            <QuestionStat
+              key={question.id}
+              question={question}
+              total={userStats[question.id]?.total || 0}
+              correct={userStats[question.id]?.correct || 0}
+              averageTime={userStats[question.id]?.averageTime || 0}
+            />
           ))}
         </>
       )}
